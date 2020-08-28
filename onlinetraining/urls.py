d2eb5098c220d734b3f1e0329002f39168926d0e
RajTagore <54748806+RajTagore@users.noreply.github.com>
2020-08-28 05:30:12 +0530

    1

--- onlinetraining/urls.py
+++ onlinetraining/urls.py
@@ -11,6 +11,12 @@ urlpatterns = [
     #re_path(r'AddClient/$', views.AddClient),
     re_path(r'pay2/$', views.Pay2),
     re_path(r'pay3TpayU/$', views.Pay3),
-    re_path(r'OTPaymentSuccess', views.OTPaymentSuccess),
-    re_path(r'OTPaymentFailure', views.OTPaymentFailure)
+    re_path(r'OTPaymentSuccess', views.OTPaymentSuccess), 
+    re_path(r'OTPaymentFailure', views.OTPaymentFailure),
+    re_path(r'Insurance/$', views.InsuranceTrainingPg),
+    re_path(r'Sales/$', views.SalesTrainingPg),
+    re_path(r'NetworkMarketing/$', views.NetworkMarketingTrainingPg),
+    re_path(r'Business/$', views.BusinessTrainingPg),
+    re_path(r'SelfDevelopment/$', views.SelfDevelopmentTrainingPg),
+    path(r'ReadMore/<CourseName>/', views.MoreAboutTraining),  
 ]
