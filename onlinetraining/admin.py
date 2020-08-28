5eaa75bc3c0f120fed245da9fc8d3daa353a827b
RajTagore <54748806+RajTagore@users.noreply.github.com>
2020-08-28 05:52:49 +0530

    brocomeon

--- onlinetraining/admin.py
+++ onlinetraining/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import OnlineTrainingProgram, OnlineTrainingApplicant
+from .models import OnlineTrainingProgram, OnlineTrainingApplicant, OnlineTrainingApplicantType, OnlineTrainingCategory
 
 
 # Register your models here.
 admin.site.register(OnlineTrainingProgram)
 admin.site.register(OnlineTrainingApplicant)
+admin.site.register(OnlineTrainingApplicantType)
+admin.site.register(OnlineTrainingCategory)
